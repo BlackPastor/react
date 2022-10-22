97e88a925b3d59e296b1b1f279c773c983c4a6f8
Serhii <sergii.ippolitov@gmail.com>
2022-10-22 21:10:36 +0300

    added hw#15

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AnakinPhoto from "./images/Anakin.jpg";
+import RayPhoto from "./images/Ray.jpg";
+import CheckMark from "./images/check-mark.svg";
+import Like from "./images/like.svg"
+import Repost from "./images/repost.svg"
+import Share from "./images/share.svg"
+import Comment from "./images/comment.svg"
+
+const ANAKIN_IMAGE = AnakinPhoto;
+
+const RAY_IMAGE = RayPhoto;
+
+const author = {
+  name: "Anakin skywalker",
+  photo: ANAKIN_IMAGE,
+  nickname: "@dart_vader",
+  content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+  image: RAY_IMAGE,
+  date: "26 feb",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img className="logo" src={author.photo}></img>
+      <div className="info">
+        <div className="header">
+        <h3>{author.name}</h3>
+        <img src={CheckMark}></img>
+        <h5>{author.nickname}</h5>
+        <h5>{author.date}</h5>
+      </div>
+      <div className="content">
+        <h4>{author.content}</h4>
+        <img src={author.image} className="gal image"></img>
+      </div>
+      <div className="footer">
+        <div className="actions">
+          <img src={Comment}></img>
+          <p>482</p>
+        </div>
+        <div className="actions">
+          <img src={Repost}></img>
+          <p>146</p>
+        </div>
+        <div className="actions">
+          <img src={Like}></img>
+          <p>887</p>
+        </div>
+        <div className="actions">
+          <img src={Share}></img>
+        </div>
+      </div>
+      </div>
+      
     </div>
   );
 }
